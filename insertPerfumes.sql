f2e8cef893c5b80b4ed281c8fcff0c0f4e745f50
Felipe <fronckowiakfelipe@gmail.com>
2024-09-29 20:18:07 -0300

    insert perfumes

---
+++ insertPerfumes.sql
@@ -0,0 +1,13 @@
+INSERT INTO perfume (nome_perfume, id_marca, ano_lançamento_perfume, id_genero, link_imagem) VALUES
+('Layton', 13, 2016, 3, 'https://fraguru.com/mdimg/perfume/375x500.39314.jpg');
+('Eros', 11, 2012, 1, 'https://fraguru.com/mdimg/perfume/375x500.16657.jpg');
+('Versace Pour Homme Dylan Blue', 11, 2016, 1, 'https://fraguru.com/mdimg/perfume/375x500.40031.jpg');
+('Spicebomb Extreme', 14, 2015, 1, 'https://fraguru.com/mdimg/perfume/375x500.30499.jpg');
+('Goddess', 14, 2023, 2, 'https://fraguru.com/mdimg/perfume/375x500.83483.jpg');
+('9pm', 16, 2020, 1, 'https://fraguru.com/mdimg/perfume/375x500.65414.jpg');
+('Jazz Club', 7, 2013, 1, 'https://fraguru.com/mdimg/perfume/375x500.20541.jpg');
+('Bleu de Chanel Eau de Parfum', 12, 2014, 1, 'https://fraguru.com/mdimg/perfume/375x500.25967.jpg');
+('Bianco Latte', 17, 2023, 3, 'https://fraguru.com/mdimg/perfume/375x500.64757.jpg');
+('Club de Nuit Intense Man', 18, 2015, 1, 'https://fraguru.com/mdimg/perfume/375x500.34696.jpg');
+('Sauvage', 8, 2015, 1, 'https://fraguru.com/mdimg/perfume/375x500.31861.jpg');
+('Sauvage Elixir', 8, 2021, 1, 'https://fraguru.com/mdimg/perfume/375x500.68415.jpg');
